77fd4f846afde90b69d2269d226d8a731abee4ac
R312r0 <vladyslav.chupovskiy@gmail.com>
2023-01-19 00:27:10 +0200

    simple swagger ui

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({title: "test@mail.com", description: "Email"})
     readonly email: string;
+    @ApiProperty({title: "abX_123-yuiL", description: "Password"})
     readonly password: string;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
 
 interface UserCreationAttribute {
     email: string,
@@ -7,12 +8,15 @@ interface UserCreationAttribute {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttribute> {
-
+    @ApiProperty({example: '1', description: "Uniq identifier"})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
+
+    @ApiProperty({example: 'test@mail.com', description: "Users email"})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
 
+    @ApiProperty({example: 'abX_123-yuiL', description: "Users password"})
     @Column({type: DataType.STRING, allowNull: false})
     password: string;
 
